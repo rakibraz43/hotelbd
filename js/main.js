2ea352a449ba32ec0cb9a351012232442ff5d4e3
Md. Rakib Mah <99381836+rakibraz43@users.noreply.github.com>
2022-03-18 18:47:25 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+  // project testimony slider code
+
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        767:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
